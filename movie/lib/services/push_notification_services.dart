109a0305a2b02f3dfb1cab3159494f9ccd727264
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-03-15 10:57:32 +0300

    Update push_notification_services.dart

--- movie/lib/services/push_notification_services.dart
+++ movie/lib/services/push_notification_services.dart
@@ -1,24 +1,39 @@
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class PushNotificationService {
-  Future<void> initialize() async {
-    await OneSignal.shared.init(
-      'YOUR_ONESIGNAL_APP_ID',
-      iOSSettings: {
-        OSiOSSettings.autoPrompt: false,
-        OSiOSSettings.inAppLaunchUrl: true,
-      },
+  static Future<void> initialize() async {
+    await OneSignal.shared.setAppId('<ONESIGNAL_APP_ID>');
+
+    await OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // Handle notification open here
+    });
+
+    await OneSignal.shared
+        .setInAppMessageClickedHandler((OSInAppMessageAction action) {
+      // Handle in app message click here
+    });
+  }
+
+  static Future<void> subscribeToTopic(String topic) async {
+    await OneSignal.shared.sendTag(topic, 'true');
+  }
+
+  static Future<void> unsubscribeFromTopic(String topic) async {
+    await OneSignal.shared.deleteTag(topic);
+  }
+
+  static Future<void> sendNotification(String title, String body,
+      {String? imageUrl, Map<String, dynamic>? data}) async {
+    final notification = OSCreateNotification(
+      playerIds: null,
+      content: body,
+      heading: title,
+      iosAttachments: imageUrl != null
+          ? {'id1': imageUrl}
+          : null, // iOS requires a unique ID for each attachment
+      data: data,
     );
-  }
-
-  Future<void> register() async {
-    await OneSignal.shared.promptUserForPushNotificationPermission();
-    final status = await OneSignal.shared.getPermissionSubscriptionState();
-    final playerId = status.subscriptionStatus.userId;
-    // Save playerId to server or local storage
-  }
-
-  Future<void> unregister() async {
-    await OneSignal.shared.setSubscription(false);
+    await OneSignal.shared.postNotification(notification);
   }
 }
