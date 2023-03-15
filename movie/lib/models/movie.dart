330865588b94f351514913147b5fa4edcca62133
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-03-15 10:46:06 +0300

    Update movie.dart

--- movie/lib/models/movie.dart
+++ movie/lib/models/movie.dart
@@ -7,11 +7,8 @@ class Movie {
   final String overview;
   final String posterPath;
   final String backdropPath;
-  final DateTime releaseDate;
-  final List<int> genreIds;
   final double voteAverage;
-  final int voteCount;
-  final String videoKey;
+  final String releaseDate;
 
   Movie({
     required this.id,
@@ -19,26 +16,7 @@ class Movie {
     required this.overview,
     required this.posterPath,
     required this.backdropPath,
-    required this.releaseDate,
-    required this.genreIds,
     required this.voteAverage,
-    required this.voteCount,
-    required this.videoKey,
+    required this.releaseDate,
   });
-
-  factory Movie.fromJson(Map<String, dynamic> json) {
-    final genreIds = List<int>.from(json['genre_ids']);
-    return Movie(
-      id: json['id'],
-      title: json['title'],
-      overview: json['overview'],
-      posterPath: json['poster_path'],
-      backdropPath: json['backdrop_path'],
-      releaseDate: DateTime.parse(json['release_date']),
-      genreIds: genreIds,
-      voteAverage: json['vote_average'].toDouble(),
-      voteCount: json['vote_count'],
-      videoKey: '',
-    );
-  }
 }
